4f852bc8213f471b1bd3ec7b74c2b2a1ad01ac80
Tony Fong <tonyfong@outlook.com>
2019-08-18 11:37:53 +0800

    add sign up

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 
-import { signInWithGoogle } from '../../firebase/firebase.util.js';
+import { auth, signInWithGoogle } from '../../firebase/firebase.util.js';
 import './sign-in.style.scss';
 
 class SignIn extends React.Component {
@@ -16,10 +16,16 @@ class SignIn extends React.Component {
         }
     }
 
-    handleSubmit = event => {
+    handleSubmit = async event => {
         event.preventDefault();
+        const { email, password } = this.state;
 
-        this.setState({email: '', password: ''})
+        try {
+            await auth.signInWithEmailAndPassword(email, password);
+            this.setState({email: '', password: ''});
+        } catch (error) {
+            console.log(error);
+        }
     }
 
     handleChange = event => {
@@ -48,12 +54,15 @@ class SignIn extends React.Component {
                         handleChange={this.handleChange}
                         label='password'
                         required />
-                    <CustomButton type='submit'>
-                        SIGN IN
-                    </CustomButton>
-                    <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
-                        SIGN IN with Google
-                    </CustomButton>
+                    <div className='buttons'>
+                        <CustomButton type='submit'>
+                            SIGN IN
+                        </CustomButton>
+                        <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+                            SIGN IN with Google
+                        </CustomButton>
+                    </div>
+
                 </form>
         </div>
         )
