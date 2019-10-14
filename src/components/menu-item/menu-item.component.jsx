cc34752c51736ccb0ec5107b1bd98e05542d3831
Tony Fong <tonyfong@outlook.com>
2019-10-14 23:15:31 +0800

    Change directory, collection into Reducer and Change Shop as object

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import { withRouter } from 'react-router-dom';
 import './menu-item.style.scss';
 
-const MenuItem = ({title, imageUrl, size, history, match}) => (
+const MenuItem = ({title, imageUrl, linkUrl, size, history, match}) => (
     <div 
         className={`${size} menu-item`}
-        onClick={() => history.push(`${match.url}${title}`)}>
+        onClick={() => history.push(`${match.url}${linkUrl}`)}>
         <div 
             className='background-image'
             style={{
--- src/page/shop/shop.component.jsx
+++ src/page/shop/shop.component.jsx
@@ -1,23 +1,14 @@
 import React from 'react';
-import SHOP_DATA from './shop.data.js';
-import CollectionPreview from '../../components/collection-preview/collection-preview.component'
+import { Route } from 'react-router-dom';
 
-class ShopPage extends React.Component {
-    constructor (props) {
-        super(props);
-        this.state = {
-            collections: SHOP_DATA
-        };
-    }
+import CollectionOverview from '../../components/collections-overview/collections-overview.component'
+import CollectionPage from '../../page/collection/collection.component';
 
-    render() {
-        const collections = this.state.collections;
-        return (
-            collections.map(({id, ...otherCollectionProps}) => (
-                <CollectionPreview key={id} {...otherCollectionProps} />
-            ))
-        )
-    }
-}
+const ShopPage = ({match}) => (
+    <div className='shop-page'>
+        <Route exact path={`${match.path}`} component={CollectionOverview}/>
+        <Route path={`${match.path}/:collectionName`} component={CollectionPage}/>
+    </div>
+);
 
 export default ShopPage;
