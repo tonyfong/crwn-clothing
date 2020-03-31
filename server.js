ec07a8fcd3d662aff8c9d778cbdaa282e4d7dc33
Tony Fong <tonyfong@outlook.com>
2020-03-31 16:42:51 +0800

    correct server.js port to PORT

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-const express = require("express")
-const cors = require("cors");
-const bodyParser = require("body-parser");
-const path = require("path");
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const path = require('path');
 
 if (process.env.NODE_ENV!=='production') require('dotenv').config();
 
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 const app = express();
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -22,9 +22,9 @@ if(process.env.NODE_ENV==='production') {
     });
 }
 
-app.listen(port, error=> {
-    if(error) throw error; 
-    console.log('Server is running on port ' + port);
+app.listen(port, error => {
+  if (error) throw error;
+  console.log('Server running on port ' + port);
 });
 
 app.post('/payment', (req, res) => {
@@ -34,12 +34,11 @@ app.post('/payment', (req, res) => {
         currency: 'usd'
     };
 
-    stripe.charges.create(body, (stripeErr, stripeRes)=> {
-        if (stripeErr) {
-            res.status(500).send({ error: stripeErr});
-        }
-
-        res.status(200).send({success: stripeRes});
-
-    })
-})
+  stripe.charges.create(body, (stripeErr, stripeRes) => {
+    if (stripeErr) {
+      res.status(500).send({ error: stripeErr });
+    } else {
+      res.status(200).send({ success: stripeRes });
+    }
+  });
+});
